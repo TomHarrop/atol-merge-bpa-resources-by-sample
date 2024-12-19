dfb4609af4488bca0ea8724cda4320c1162da99b
Tom Harrop <twharrop@gmail.com>
2024-12-19 11:34:59 +1100

    dump dp contents with ckanapi

--- workflow/scripts/parse_bpa_json.py
+++ workflow/scripts/parse_bpa_json.py
@@ -8,9 +8,9 @@ json_file = "datasets.jsonl.gz"
 
 
 def read_json(json_file):
-    '''
+    """
     Each line is a Package.
-    '''
+    """
     with gzip.open(json_file, "rb") as f:
         data = jsonlines.Reader(f)
         for line in data:
@@ -19,6 +19,26 @@ def read_json(json_file):
 
 data = read_json(json_file)
 
-package  = next(data)
+sample_id_dict = {}
+bioplatforms_sample_id_dict = {}
+equivalent_samples = []
 
+for i, package in enumerate(data):
+    bp_id = False
+    if "bioplatforms_sample_id" in package:
+        bp_id = True
+        my_id = package["bioplatforms_sample_id"]
+        if my_id in bioplatforms_sample_id_dict:
+            bioplatforms_sample_id_dict[my_id].append(package)
+        else:
+            bioplatforms_sample_id_dict[my_id] = [package]
+    if "sample_id" in package:
+        my_id = package["sample_id"]
+        if bp_id:
+            equivalent_samples.append((sample_id, bioplatforms_sample_id))
+        if my_id in sample_id_dict:
+            sample_id_dict[my_id].append(package)
+        else:
+            sample_id_dict[my_id] = [package]
 
+equivalent_samples
